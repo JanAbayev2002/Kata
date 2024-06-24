bcfbbce041bd5616e552ccc41778c320a74c4083
JanAbayev2002 <quiet.workplace.amb@gmail.com>
2024-06-24 16:32:46 +0300

    Получение данных от пользователя

--- kata.go
+++ kata.go
@@ -1,7 +1,15 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
 
-func main () {
-	fmt.Println("Good")
+func main() {
+	fmt.Printf("Задание для втупление в Kata\n")
+	fmt.Println("Введите выражение в формате x + y:")
+	getting_an_expression := bufio.NewReader(os.Stdin)
+	resulting_expression, _ := getting_an_expression.ReadString('\n')
+	fmt.Println(resulting_expression)
 }
