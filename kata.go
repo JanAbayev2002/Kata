85667fc2aea939d8c886f73f3b556658df7e776d
JanAbayev2002 <quiet.workplace.amb@gmail.com>
2024-06-24 16:45:53 +0300

    Добавлен выход из проекта по ключевому слову

--- kata.go
+++ kata.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"strings"
 	"bufio"
 	"fmt"
 	"os"
@@ -9,7 +10,15 @@ import (
 func main() {
 	fmt.Printf("Задание для втупление в Kata\n")
 	fmt.Println("Введите выражение в формате x + y:")
+
 	getting_an_expression := bufio.NewReader(os.Stdin)
 	resulting_expression, _ := getting_an_expression.ReadString('\n')
+	resulting_expression = strings.TrimSpace (resulting_expression)
 	fmt.Println(resulting_expression)
+
+	if resulting_expression == "exit" {
+		fmt.Println ("Вы покинули проект")
+	}
+
+	
 }
